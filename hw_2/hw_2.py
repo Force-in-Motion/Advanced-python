7676c5c721a883325895b77f856093d15ea85ee2
Force-in-Motion <canzdao@gmail.com>
2024-10-23 20:53:33 +0300

    hw_2 confirm

--- hw_2/hw_2.py
+++ hw_2/hw_2.py
@@ -4,81 +4,81 @@ import math
 import random
 
 
-# lst_str = ['22', '49', '89', '9']
-#
-# list_nums = list(map(lambda x: int(x) if x.isdigit else TypeError('Не верный тип данных'), lst_str))
+lst_str = ['22', '49', '89', '9']
+
+list_nums = list(map(lambda x: int(x) if x.isdigit else TypeError('Не верный тип данных'), lst_str))
 
 # ==================================== 2 =================================================================
 
-# lst_numbers = [1, 2, 3, 4, 5, 6]
-#
-# list_even = list(filter(lambda x:  x % 2 == 0, lst_numbers))
+lst_numbers = [1, 2, 3, 4, 5, 6]
+
+list_even = list(filter(lambda x:  x % 2 == 0, lst_numbers))
 
 # ======================================= 3 ==============================================================
 
-# lst_numbers = [1, 2, 3, 4, 5, 6]
-#
-# result_lst = list(map(lambda x: x**2 if isinstance(x, (int, float))  else TypeError('Не верный тип данных'), lst_numbers))
+lst_numbers = [1, 2, 3, 4, 5, 6]
+
+result_lst = list(map(lambda x: x**2 if isinstance(x, (int, float))  else TypeError('Не верный тип данных'), lst_numbers))
 
 # ======================================= 4 ==============================================================
 
-# lst_str = ["cat", "elephant", "dog", "tiger"]
-#
-# result_lst = list(filter(lambda x: len(x) > 3, lst_str))
+lst_str = ["cat", "elephant", "dog", "tiger"]
+
+result_lst = list(filter(lambda x: len(x) > 3, lst_str))
 
 # ======================================= 5 ==============================================================
 
-# lst_numbers = [1, 2, 3, 4]
-#
-# result = reduce(lambda x, y: x*y if isinstance(x, (int, float)) and isinstance(y, (int, float)) else TypeError('Не верный тип данных'), lst_numbers)
+lst_numbers = [1, 2, 3, 4]
+
+result = reduce(lambda x, y: x*y if isinstance(x, (int, float)) and isinstance(y, (int, float)) else TypeError('Не верный тип данных'), lst_numbers)
 
 # ======================================= 6 ==============================================================
 
-# lst_str = ["hello", "world", "Python"]
-#
-# result_lst = list(map(lambda x: len(x) if isinstance(x, str) else TypeError('Не верный тип данных'), lst_str))
+lst_str = ["hello", "world", "Python"]
+
+result_lst = list(map(lambda x: len(x) if isinstance(x, str) else TypeError('Не верный тип данных'), lst_str))
 
 # ======================================= 7 ==============================================================
 
-# lst_str = ["apple", "banana", "pear", "strawberry"]
-#
-# result = max(map(lambda x: len(x) if isinstance(x, str) else TypeError('Не верный тип данных'), lst_str))
+lst_str = ["apple", "banana", "pear", "strawberry"]
+
+result = max(map(lambda x: len(x) if isinstance(x, str) else TypeError('Не верный тип данных'), lst_str))
 
 # ======================================= 8 ==============================================================
 
-# lst_str = ["hello", "world"]
-#
-# result_lst = list(map(lambda x: x.upper() if isinstance(x, str) else TypeError('Не верный тип данных'), lst_str))
+lst_str = ["hello", "world"]
+
+result_lst = list(map(lambda x: x.upper() if isinstance(x, str) else TypeError('Не верный тип данных'), lst_str))
 
 # ======================================= 9 ==============================================================
 
-# lst_str = ["1", "2", "3", "4"]
-#
-# result_lst = list(map(lambda x: int(x)**2, list(filter(lambda x: int(x) % 2 == 0 if x.isdigit else TypeError('Не верный тип данных'), lst_str))))
+lst_str = ["1", "2", "3", "4"]
+
+result_lst = list(map(lambda x: int(x)**2, list(filter(lambda x: int(x) % 2 == 0 if x.isdigit else TypeError('Не верный тип данных'), lst_str))))
 
 # ======================================= 10 ==============================================================
 
-# lst_numbers = [-2, 3, -4, 5, 6]
-#
-# result = reduce(lambda x, y: x*y, list(filter(lambda x: x >= 0 if isinstance(x, (int, float)) else TypeError('Не верный тип данных'), lst_numbers)))
+lst_numbers = [-2, 3, -4, 5, 6]
+
+result = reduce(lambda x, y: x*y, list(filter(lambda x: x >= 0 if isinstance(x, (int, float)) else TypeError('Не верный тип данных'), lst_numbers)))
 
 # ======================================= 11 ==============================================================
 
-# lst_str = ["apple", "banana", "orange", "grape"]
-#
-# result_lst = list(map(lambda x: len(x), list(filter(lambda x: x.startswith(('a', 'e', 'i', 'o', 'u')), lst_str))))
+lst_str = ["apple", "banana", "orange", "grape"]
+
+result_lst = list(map(lambda x: len(x), list(filter(lambda x: x.startswith(('a', 'e', 'i', 'o', 'u')), lst_str))))
 
 # ======================================= 12 ==============================================================
 
-# lst_numbers = [2, 3, 4, 5, 6]
-#
-# result = reduce(lambda x, y: math.factorial(x) * math.factorial(y), list(filter(lambda x: x % 2 == 0 if isinstance(x, (int, float)) else TypeError('Не верный тип данных'), lst_numbers)))
+lst_numbers = [2, 3, 4, 5, 6]
+
+result = reduce(lambda x, y: math.factorial(x) * math.factorial(y), list(filter(lambda x: x % 2 == 0 if isinstance(x, (int, float)) else TypeError('Не верный тип данных'), lst_numbers)))
 
 # ======================================= 13 ==============================================================
 
-# lst_str = ["hello", "world", "Python", "is", "great"]
-#
-# result = ' '.join(list(map(lambda x: x.upper(), list(filter(lambda x: len(x) % 2 == 0, lst_str)))))
+lst_str = ["hello", "world", "Python", "is", "great"]
+
+result = ' '.join(list(map(lambda x: x.upper(), list(filter(lambda x: len(x) % 2 == 0, lst_str)))))
 
 
 # ======================================= Генераторы 1 =====================================================
